56b5a424e0fb14e987b83a7cf0078b1bdb4e4392
Ana Paula <anapaulabernardo1703@gmail.com>
2024-04-16 14:07:43 -0300

    Possible moves of a piece

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,12 +1,12 @@
 package application;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
-
-import javax.security.sasl.SaslClient;
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
 public class UI {
 
@@ -31,28 +31,35 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
-    public static ChessPosition readChessPosition(Scanner sc){
-        try{
+    // https://stackoverflow.com/questions/2979383/java-clear-the-console
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public static ChessPosition readChessPosition(Scanner sc) {
+        try {
             String s = sc.nextLine();
             char column = s.charAt(0);
             int row = Integer.parseInt(s.substring(1));
             return new ChessPosition(column, row);
         }
-        catch (RuntimeException e){
+        catch (RuntimeException e) {
             throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
         }
     }
 
-    public static void printBoard(ChessPiece[][] pieces){
-        for(int i=0; i<pieces.length; i++){
+    public static void printBoard(ChessPiece[][] pieces) {
+        for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
-            for(int j=0; j<pieces.length; j++){
+            for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j]);
             }
             System.out.println();
         }
         System.out.println("  a b c d e f g h");
     }
+
     private static void printPiece(ChessPiece piece) {
         if (piece == null) {
             System.out.print("-");
@@ -67,4 +74,6 @@ public class UI {
         }
         System.out.print(" ");
     }
+
+
 }
